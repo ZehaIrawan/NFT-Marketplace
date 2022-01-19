2d48c64b1fd21adcce5a4359b38ccb9533309676
Zeha Irawan <zehairawan@gmail.com>
2022-01-19 09:26:57 +0800

    fetch image from pexels api

---
+++ types.ts
@@ -0,0 +1,12 @@
+
+export interface Photo {
+  id: number;
+  photographer: string,
+    photographer_url: string,
+    photographer_id: number,
+    avg_color: string,
+    src: {
+      original: string,
+    },
+    alt:string
+}
